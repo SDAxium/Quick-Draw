095b073a666d8c8984a860373130156c9e684b74
SDAxium <sdaxium@gmail.com>
2022-07-02 20:57:50 -0400

    Update 0.01E
    Yeah I lied. Git didn't bless as much as I thought it did. A bunch of things were lost between the last merge and revert. Live and Learn
    
    Created ray interactor for UI(transparent when not on UI, only interacts with UI)
    Created Prefabs for player, gun model
    Created glowing material for target(prefab will be made eventually for testing
    
    Update game manager script to move between scenes but as it stands only one scene exists. Scenes will be preloaded on menu scene. When moving to another scene, all other scenes will be unloaded because that probably puts less of a load on the VR headset
    
    Added post processing to sample scene. Bloom is used to acheive glowing effect of target.

--- Quick Draw/Assets/Scripts/GameManager.cs
+++ Quick Draw/Assets/Scripts/GameManager.cs
@@ -1,22 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
+    public GameManager instance;
     public GameObject player;
+
+    public bool sceneTravel = false;
+    private AsyncOperation _scene;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-       // InvokeRepeating(nameof(MehFire),3f,10f);
+        // InvokeRepeating(nameof(MehFire),3f,10f);
+        _scene = SceneManager.LoadSceneAsync(1);
+        _scene.allowSceneActivation = false;
+        
+        // InvokeRepeating(nameof(MehFire),3f,10f);
     }
     
     void Update()
     {
+        if (sceneTravel)
+        {
+            _scene.allowSceneActivation = true;
+        }
         
         
     }
+
+    public void LoadNextScene()
+    {
+        sceneTravel = true;
+    }
     
+    public void Exit()
+    {
+        Application.Quit();
+    }
     /*void MehFire()
     {
         GameObject target = GameObject.Find("Target Object");
